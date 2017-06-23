2950ecd97b5a5d2585f89507ff05f3f0cc20331c
stefani spayde <spayde.stefani@gmail.com>
2017-06-23 15:13:19 -0500

    completed logic for real

--- js/main.js
+++ js/main.js
@@ -24,41 +24,36 @@ var cards = [
 var cardsInPlay = [];
 
 function checkForMatch() {
-  if(cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-
+     if(cardsInPlay.length === 2) {
+      if(cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
   } else {
     alert("Sorry, try again.");
     }
-}
-
- function flipCard (cardId){
-  var cardOne = cards[0];
-  cardsInPlay.push('cardOne');
-  var cardTwo = cards[2];
-  cardsInPlay.push('cardTwo');
-
-  if(cardsInPlay.length === 2) {
-    checkForMatch();
   }
+};
+
+var flipCard = function() {
+  var cardId = this.getAttribute("data-id");
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
-  console.log("User flipped " + cards[cardId].image);
-  console.log("User flipped " + cards[cardId].suit);
-}
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  this.setAttribute("src", cards[cardId].cardImage);
+    if(cardsInPlay.length === 2) {
+      checkForMatch();
+    }
 
-function createBoard() {
-  for(var i = 0; i < cards.length; i++) {
+};
+
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++) {
     var cardElement = document.createElement('img');
-    cardElement.setAttribute('src','images/back.png');
+    cardElement.setAttribute('src', 'images/back.png');
     cardElement.setAttribute('data-id', i);
-
+    document.getElementById('game-board').appendChild(cardElement);
     cardElement.addEventListener('click', flipCard);
-      document.getElementById('game-board').appendChild(cardElement);
-  }
-}
+    }
+};
 
 createBoard();
-
-flipCard(0);
-flipCard(2);
